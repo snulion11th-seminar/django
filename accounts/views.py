3e2980d7dc35e175ca304c5b3dfd3fae8d538712
HYUNHONOH98 <hyunho@Hyunhoui-MacBookPro.local>
2023-03-01 00:56:19 +0900

    feature / accounts login logout view added

--- accounts/views.py
+++ accounts/views.py
@@ -11,11 +11,33 @@ class SignupView(APIView):
         try:
             assert(request.data['password1'] == request.data['password2'])
             user = User.objects.create(
+                email=request.data["email"],
                 username=request.data['username'],
                 password=request.data['password1']
                 )
             auth.login(request, user)
             user_serializer = UserSerializer(user)
             return Response(user_serializer.data)
+        except Exception as e:
+            return Response({"error":f"signup error : {e}"})
+        
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            user = User.objects.get(
+                username=request.data['username'],
+                password=request.data['password']
+            )
+            auth.login(request, user)
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data)
+        except Exception as e:
+            return Response({"error":f"login error : {e}"})
+        
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            auth.logout(request)
+            return Response({"msg":f"{request.user.username} 이 logout 되었습니다."})
         except Exception as e:
             return Response({"error":f"{e}"})
