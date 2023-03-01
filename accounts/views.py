88c5682777715179113c8aac63c88c0dd883ba86
dev_Nhh <97432152+HYUNHONOH98@users.noreply.github.com>
2023-03-01 20:51:51 +0900

    Feature/jwttoken (#10)
    * requirements txt changed && views urls added
    
    * jwt token used
    
    * fix / unused imports
    
    ---------
    
    Co-authored-by: HYUNHONOH98 <hyunho@Hyunhoui-MacBookPro.local>

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
-<<<<<<< Updated upstream
-
-=======
-import requests, json
->>>>>>> Stashed changes
+from rest_framework_simplejwt.tokens import RefreshToken
 
 ############## no jwt ###################
 class SignupView(APIView):
@@ -28,8 +24,6 @@ class SignupView(APIView):
             return Response({"error":f"signup error : {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
 class SigninView(APIView):
-<<<<<<< Updated upstream
-=======
     def post(self, request):
         try:
             user = User.objects.get(
@@ -52,6 +46,11 @@ class LogoutView(APIView):
         
         
 ################ use jwt ###################
+def generate_token(user:User) -> str:
+    token = RefreshToken.for_user(user)
+    refresh_token, access_token = str(token), str(token.access_token)
+    return refresh_token, access_token
+
 class SignupView(APIView):
     def post(self, request):
         try:
@@ -60,35 +59,33 @@ class SignupView(APIView):
                 email=request.data["email"],
                 username=request.data['username'],
                 password=request.data['password1']
-                )
-            
-            user_serializer = UserSerializer(user)
-            url = "http://127.0.0.1:8000/api/api/token"
-            response = requests.request("post",url, json=request.data)
-            return Response(response.json())
+                )            
+            refresh_token, access_token = generate_token(user)
+            data = UserSerializer(user).data
+            data['token']={"access":access_token, "refresh":refresh_token}
+            return Response(data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error":f"signup error : {e}"})
         
 class SigninView(APIView):
->>>>>>> Stashed changes
     def post(self, request):
         try:
             user = User.objects.get(
                 username=request.data['username'],
                 password=request.data['password']
             )
-            print(request.user)
-            auth.login(request, user)
-            user_serializer = UserSerializer(user)
-            return Response(user_serializer.data, status=status.HTTP_200_OK)
+            refresh_token, access_token = generate_token(user)
+            data = UserSerializer(user).data
+            data['token']={"access": access_token, "refresh":refresh_token}
+            return Response(data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error":f"login error : {e}"}, status=status.HTTP_400_BAD_REQUEST)
         
 class LogoutView(APIView):
     def post(self, request):
         try:
-            signin_username = request.user
-            auth.logout(request)
-            return Response({"msg":f"{signin_username} 이 logout 되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+            token = request.data['refresh']
+            RefreshToken(token).blacklist()
+            return Response({"msg":f"logout 되었습니다."}, status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({"error":f"{e}"}, status=status.HTTP_401_UNAUTHORIZED)
