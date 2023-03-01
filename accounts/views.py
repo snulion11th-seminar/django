5f0f79402cb905f1c513b0239683a458de4bce1f
HYUNHONOH98 <hyunho@Hyunhoui-MacBookPro.local>
2023-03-01 20:42:04 +0900

    fix / unused imports

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
-import requests, json
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
