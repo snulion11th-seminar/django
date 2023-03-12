952dd5e63aaf4fc2986b867d0dda5539bceaf5be
HYUNHONOH98 <hyunho@Hyunhoui-MacBookPro.local>
2023-03-12 21:05:32 +0900

    account review reflected on files

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-
+ 
 app_name = 'account'
 urlpatterns = [
     # FBV url path
--- account/views.py
+++ account/views.py
@@ -4,40 +4,55 @@ from django.contrib import auth
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import UserSerializer,UserProfileSerializer
 from .models import UserProfile
-from rest_framework_simplejwt.tokens import RefreshToken
 
 ############## no jwt ###################
 class SignupView(APIView):
     def post(self, request):
-        assert(request.data['password1'] == request.data['password2'])
-        user = User.objects.create(
-            email=request.data["email"],
-            username=request.data['username'],
-            password=request.data['password1']
-            )
+        email=request.data.get("email")
+        username=request.data.get('username')
+        password=request.data.get('password')
+        college=request.data.get('college')
+        major=request.data.get('major')
+        if not email or not username or not password:
+            return Response({"detail": "[email, password, username] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user = User.objects.create(
+                email=request.data["email"],
+                username=request.data['username'],
+                password=request.data['password']
+                )
         auth.login(request, user)
         serialized_data = UserSerializer(user).data
         return Response(serialized_data, status=status.HTTP_201_CREATED)
         
 class SigninView(APIView):
     def post(self, request):
-        user = User.objects.get(
-            username=request.data['username'],
-            password=request.data['password']
-        )
+        try:
+            user = User.objects.get(
+                username=request.data['username'],
+                password=request.data['password']
+            )
+        except:
+            return Response({"detail": "아이디 또는 비밀번호를 확인해주세요."}, status=status.HTTP_400_BAD_REQUEST)
         auth.login(request, user)
         serialized_data = UserSerializer(user).data
-        return Response(serialized_data)
+        return Response(serialized_data, status=status.HTTP_200_OK)
         
 class LogoutView(APIView):
     def post(self, request):
+        try:
+            user = request.user
+        except:
+            return Response({"detail": "로그인 후 다시 시도해주세요."}, status=status.HTTP_401_UNAUTHORIZED)
         auth.logout(request)
-        return Response({"msg":f"{request.user.username} 이 logout 되었습니다."})
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
         
-################ use jwt ###################
+# ################ use jwt ###################
 def generate_token_in_serialized_data(user:User) -> str:
     token = RefreshToken.for_user(user)
     refresh_token, access_token = str(token), str(token.access_token)
@@ -52,7 +67,9 @@ class SignupView(APIView):
         password=request.data.get('password')
         college=request.data.get('college')
         major=request.data.get('major')
-        if email and username and password:
+        if not email or not username or not password:
+            return Response({"detail": "[email, password, username] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
             user = User.objects.create(
                 email=email,
                 username=username,
@@ -65,8 +82,6 @@ class SignupView(APIView):
             )
             serialized_data = generate_token_in_serialized_data(user)
             return Response(serialized_data, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"detail": "[email, password, username] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
         
 class SigninView(APIView):
     def post(self, request):
@@ -91,12 +106,17 @@ class LogoutView(APIView):
 
 class ProfileUpdateView(APIView):
     def patch(self, request):
-        user = request.user
-        profile = UserProfile.objects.filter(user=user)
+        try:
+            user = request.user
+        except:
+            return Response({"detail": "로그인 후 다시 시도해주세요."}, status=status.HTTP_401_UNAUTHORIZED)
+        profile = UserProfile.objects.get(user=user)
         serializer = UserProfileSerializer(profile, data=request.data, partial=True) 
-        if serializer.is_valid():
+        if not serializer.is_valid():
+            return Response({"detail": "data validation error"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 
         
