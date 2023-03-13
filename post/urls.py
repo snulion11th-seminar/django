e04c761621b44b713ea816ccf098cfefcaae11ef
HYUNHONOH98 <hyunho@Hyunhoui-MacBookPro.local>
2023-03-13 22:48:01 +0900

    break change: CBV only, FBV moved to week5/inclass

--- post/urls.py
+++ post/urls.py
@@ -5,10 +5,7 @@ from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView
 
 app_name = 'post'
 urlpatterns = [
-    # FBV url path
-    path("register_post/", CreatePostView),
-    path("see_post/", ReadAllPostView),
-    # CBV url path
+
     path("", PostListView.as_view()),
     path("<int:post_id>/", PostDetailView.as_view())
 ]
