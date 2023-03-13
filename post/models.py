9dbe4baa0b0f3599a126bd0a207996fa3d71d3dc
young924 <ahenaka9@gmail.com>
2023-03-13 11:45:14 +0900

    [feat] fix author field of Post and make migration

--- post/models.py
+++ post/models.py
@@ -1,13 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 # Create your models here.
 from django.utils import timezone
+
 
 class Post(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
