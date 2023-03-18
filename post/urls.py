e0311bdc229b5650cd5a4292214f1a95dd64bc02
BDlhj <akk808@naver.com>
2023-03-18 23:08:05 +0900

    feat: Add Like url

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView
+from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView, LikeView
 
 app_name = 'post'
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path("see_post/", ReadAllPostView),
     # CBV url path
     path("", PostListView.as_view()),
-    path("<int:post_id>/", PostDetailView.as_view())
+    path("<int:post_id>/", PostDetailView.as_view()),
+    # 7th week(ManyToMany Field_Like)
+    path("<int:post_id>/like/", LikeView.as_view()),
 ]
