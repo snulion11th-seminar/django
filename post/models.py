f4e29dba07929addb97fbf914afa2ae4735ab854
BDlhj <akk808@naver.com>
2023-03-18 23:07:53 +0900

    feat: Add Like model

--- post/models.py
+++ post/models.py
@@ -9,6 +9,13 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+    like_users = models.ManyToManyField(User, blank=True, related_name='like_posts', through='Like')
 
     def __str__(self):
         return self.title
+
+# 7th week(ManyToMany Field_Like)
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
