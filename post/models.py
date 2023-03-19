ecbe5bfc8394a04726332f97132f5d260097b55e
BDlhj <akk808@naver.com>
2023-03-20 01:15:58 +0900

    feat: Add tags field in post model

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 from django.utils import timezone
+from tag.models import Tag
 
 
 class Post(models.Model):
@@ -9,6 +10,8 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
+        
