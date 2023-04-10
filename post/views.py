f34acd4b45a8fcdda4c25b7605ea252b98834b76
Jaewon <tjrwodnjs99@gmail.com>
2023-04-10 19:48:06 +0900

    ...

---
+++ post/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from .models import Post
+
+from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .serializers import PostSerializer
+
+class PostListView(APIView):
+
+    def get(self, request): 
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+
+    def post(self, request):
+        title = request.data.get('title')
+        content = request.data.get('content')
+        if not title or not content:
+            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        post = Post.objects.create(title=title, content=content)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def patch(self, request):
+        return Response("!!")
+
+class PostDetailView(APIView):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def delete(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def patch(self, request, post_id):
+        title = request.data.get('title')
+        content = request.data.get('content')
+        if not title or not content:
+            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        post.title = title
+        post.content = content
+        post.save()
+        return Response(PostSerializer(post).data)
+    
+@api_view(['POST'])
+def CreatePostView(request):
+    title = request.data.get('title')
+    content = request.data.get('content')
+    post = Post.objects.create(title=title, content=content)
+    return Response({"msg":f"'{post.title}'이 생성되었어요!"})
+
+@api_view(['GET'])
+def ReadAllPostView(request):
+    posts = Post.objects.all()
+    contents = [{post.title:post.content} for post in posts]
+    return Response({"posts":contents})
