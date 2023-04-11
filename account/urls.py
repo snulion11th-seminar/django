80b8ed5e4558fda87dba9232415a6b9b97c9bf1b
unknownburphy <chrisandjj1218@gmail.com>
2023-04-11 21:11:58 +0900

    feat: make regeneration of access token

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SignupView, SigninView, LogoutView
+from .views import SignupView, SigninView, LogoutView, RefreshView
 
 
 app_name = 'account'
 urlpatterns = [
     path("signup/", SignupView.as_view()),
     path("signin/", SigninView.as_view()),
-    path("logout/", LogoutView.as_view())
+    path("logout/", LogoutView.as_view()),
+    path("refresh/", RefreshView.as_view())
 ]
--- account/views.py
+++ account/views.py
@@ -10,6 +10,8 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+# 3
+from rest_framework_simplejwt.tokens import AccessToken
 
 # view
 
@@ -77,3 +79,32 @@ class LogoutView(APIView):
             return Response({"detail": "로그인 후 다시 시도해주세요."}, status=status.HTTP_401_UNAUTHORIZED)
         RefreshToken(request.data['refresh']).blacklist()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+def regenerate_access_token_on_response_cookie(user: User, refresh_token: RefreshToken) -> Response:
+    try:
+        refresh_token.check_exp()
+        # new_access_token = refresh_token.access_token <- 기존의 refresh_token에서부터 만들고 싶음... 이게 아닌가?
+        new_access_token = AccessToken.for_user(user)
+        res = Response({"detail": "access token regenerated"},
+                       status=status.HTTP_200_OK)
+        res.set_cookie('access_token', value=str(
+            new_access_token), httponly=True)
+        return res
+    except:
+        Response({"detail": "Refresh token expired"},
+                 status=status.HTTP_400_BAD_REQUEST)
+
+
+class RefreshView(APIView):
+    def post(self, request):
+        try:
+            user = User.objects.get(
+                username=request.data['username'])
+        except:
+            return Response({"detail": "check your id"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            refresh_token = RefreshToken(request.data['refresh'])
+        except:
+            return Response({"detail": "wrong refresh token"}, status=status.HTTP_400_BAD_REQUEST)
+        return regenerate_access_token_on_response_cookie(user, refresh_token)
