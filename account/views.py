f9f1163f91d4c20fb70cef7dc869db28b03f3362
eojin03 <elena1014@naver.com>
2023-04-12 00:44:28 +0900

    feat: assignment6

---
+++ account/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+
+# Create your views here.
+#### 1
+from django.contrib.auth.models import User
+from .models import UserProfile
+from .serializers import UserSerializer,UserProfileSerializer
+#### 2
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+#### view
+def generate_token_in_serialized_data(user:User, user_profile:UserProfile) -> UserSerializer.data:
+    token = RefreshToken.for_user(user)
+    refresh_token, access_token = str(token), str(token.access_token)
+    serialized_data = UserProfileSerializer(user_profile).data
+    serialized_data['token']={"access":access_token, "refresh":refresh_token}
+    return serialized_data
+
+def set_token_on_response_cookie(user: User, user_profile:UserProfile) -> Response:
+    token = RefreshToken.for_user(user)
+    user_profile = UserProfile.objects.get(user=user)
+    user_profile_serializer = UserProfileSerializer(user_profile)
+    res = Response(user_profile_serializer.data, status=status.HTTP_200_OK)
+    res.set_cookie('refresh_token', value=str(token), httponly=True)
+    res.set_cookie('access_token', value=str(token.access_token), httponly=True)
+    return res
+
+class SignupView(APIView):
+    def post(self, request):
+        college=request.data.get('college')
+        major=request.data.get('major')
+#### 3
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid(raise_exception=True):
+            user = user_serializer.save()
+            
+        user_profile = UserProfile.objects.create(user=user, college=college, major=major)
+#### 4
+        return set_token_on_response_cookie(user, user_profile)
+
+#### 5
+        return Response(serialized_data, status = status.HTTP_201_CREATED)
+
+class SigninView(APIView):
+    def post(self, request):
+        try:
+            user = User.objects.get(
+                username=request.data['username'],
+                password=request.data['password']
+            )
+
+            user_profile = UserProfile.objects.get(user=user)
+        except:
+            return Response({"detail": "아이디 또는 비밀번호를 확인해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+        return set_token_on_response_cookie(user, user_profile)
+
+class LogoutView(APIView):
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response({"detail": "로그인 후 다시 시도해주세요."}, status=status.HTTP_401_UNAUTHORIZED)
+        RefreshToken(request.data['refresh']).blacklist()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RefreshView(APIView):
+    def post(self, request):
+        refresh_token = request.data.get('refresh')
+        try:
+            token = RefreshToken(refresh_token)
+            new_access_token = str(token.access_token)
+        except:
+            return Response({"detail": 'Invalid Refresh Token.'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        return Response({'access': new_access_token}, status=status.HTTP_200_OK)
+        
---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView
+
+app_name = 'post'
+urlpatterns = [
+    # FBV url path
+    path("register_post/", CreatePostView),
+    path("see_post/", ReadAllPostView),
+    path("", PostListView.as_view()),
+    path("<int:post_id>/", PostDetailView.as_view())
+]
