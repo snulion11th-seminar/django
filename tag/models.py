d3423e2f90783facdb32a2b9db8e8557f54c68c1
BDlhj <akk808@naver.com>
2023-04-15 16:26:35 +0900

    feat: Add null option into author field of Tag model

--- tag/models.py
+++ tag/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Tag(models.Model):
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   content = models.TextField()
