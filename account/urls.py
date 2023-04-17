9b5cb4d2dc59586ff397308285a4c0b974cd84e2
minhouu <tortor1403@gmail.com>
2023-04-17 10:40:41 +0900

    [feat] sign in, login, logout, refresh access token functions are created

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import SigninView, SignupView, LogoutView, ReissueAccessTokenView
+
+app_name = 'account'
+urlpatterns = [
+    path("signup/", SignupView.as_view()),
+    path("signin/", SigninView.as_view()),
+    path("logout/", LogoutView.as_view()),
+    path("refresh/", ReissueAccessTokenView.as_view())
+]
