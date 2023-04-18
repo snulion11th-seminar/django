053633ac35fa2fda3a6eeed3a0ace4970c9e4f5c
young924 <ahenaka9@gmail.com>
2023-04-19 00:12:49 +0900

    fix: CommentSerializer

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 
-from account.serializers import UserSerializer
-
 from .models import Comment
 
 
 class CommentSerializer(ModelSerializer):
-    author = UserSerializer(read_only=True)
     class Meta:
         model = Comment
-        fields = ["content", "author", "created_at"]
+        fields = "__all__"
