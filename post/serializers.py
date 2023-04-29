e4341bd8a5dbb40b42bf2e53e3dd43d83fa73d42
Junyoung <netamijr@gmail.com>
2023-04-29 18:08:02 +0900

    [feature] Like, Tag

--- post/serializers.py
+++ post/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+from account.serializers import UserIdUsernameSerializer
 from .models import Post
+from tag.serializers import TagSerializer
 
 class PostSerializer(ModelSerializer):
+    author = UserIdUsernameSerializer(read_only = True)
+    tags = TagSerializer(many=True, read_only=True)
     class Meta:
         model = Post
         fields = "__all__"
--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView
+from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView, LikeView
 
 
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("see_post/", ReadAllPostView, name="get"),
     # CBV url path
     path("", PostListView.as_view()), 
-    path("<int:post_id>/", PostDetailView.as_view())
+    path("<int:post_id>/", PostDetailView.as_view()),
+    path("<int:post_id>/like/", LikeView.as_view()),
 ]
