7e0274f4402e96fe20caa3d5df7a3d308d56be76
hochan <hochan0115@gmail.com>
2023-05-01 19:21:41 +0900

    added tag app

---
+++ tag/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import TagDetailView, TagListView
+
+app_name = 'tag'
+urlpatterns = [
+    path("", TagListView.as_view()),
+    path("<int:tag_id>/", TagDetailView.as_view()),
+]
