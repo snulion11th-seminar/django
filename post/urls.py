902b574705047b9aa64e5e33933061cf02acc9d0
eojin03 <elena1014@naver.com>
2023-05-02 19:09:37 +0900

    w7 seminar

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView
+from .views import ReadAllPostView, CreatePostView, PostListView, PostDetailView, LikeView
 
 app_name = 'post'
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path("register_post/", CreatePostView),
     path("see_post/", ReadAllPostView),
     path("", PostListView.as_view()),
-    path("<int:post_id>/", PostDetailView.as_view())
-]
+    path("<int:post_id>/", PostDetailView.as_view()),
+    path("<int:post_id>/like/", LikeView.as_view())
+]  
+
