b48abb56a8f2856e169a0b4730c26f1c792cd265
eojin03 <elena1014@naver.com>
2023-05-23 19:12:22 +0900

    세미나 후 저장

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import SigninView, LogoutView, SignupView, TokenRefreshView
+from .views import SigninView, LogoutView, SignupView, TokenRefreshView, UserInfoView
 
- 
+
 app_name = 'account'
 urlpatterns = [
     # FBV url path
     path("signup/", SignupView.as_view()),
     path("signin/", SigninView.as_view()),
     path("logout/", LogoutView.as_view()),
-    path('refresh/', TokenRefreshView.as_view())   
+    path('refresh/', TokenRefreshView.as_view()),
+    path("info/", UserInfoView.as_view()),
 ]
