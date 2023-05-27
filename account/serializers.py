78b2dcb85cf0589737e908e7715ab3e4367762ca
whwoohw <woojoo1114@naver.com>
2023-05-27 14:01:55 +0900

    fix

--- account/serializers.py
+++ account/serializers.py
@@ -6,7 +6,7 @@ from .models import UserProfile
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "password", "email"]
+        fields = ["id", "username", "password", "email"]
 
 
 class UserProfileSerializer(ModelSerializer):
